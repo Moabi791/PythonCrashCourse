4fa1afc91ffd57aa4419c524a94632aa78e3f9a1
Moabi <moabilitsoane@gmail.com>
2021-09-07 10:52:04 +0200

    first commit

---
+++ Tuples/Tuples.py
@@ -0,0 +1,38 @@
+our_tuple = 1,2,3,"A","B","C"
+type(our_tuple)
+
+our_tuple =(1,2,3,"A","B","C")
+type(our_tuple)
+
+our_tuple[0:3]
+(1,2,3)
+our_list = [1,2,3,4,5,6,7]
+our_list[2] = 100
+our_list
+[1,2,100,4,5,6,7]
+
+our_tuple
+(1,2,3,'A','B','C')
+"1234567"
+
+A = [1,2,3]
+tuple(A)
+(1,2,3)
+A = tuple(A)
+A
+(A,B,C) = 1,2,3
+A = 1
+B = 2
+C = 3
+
+D,E,F = [1,2,3]
+D = 1
+E = 2
+F = 3
+G,H,I ="789"
+G
+'7'
+H
+'8'
+I
+'9'
